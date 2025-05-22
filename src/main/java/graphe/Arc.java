8acbb922149264ed59a25076c2862c7cecb3322b
Kartedian <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-05-22 09:05:10 +0200

    Creation class Arc

---
+++ src/main/java/graphe/Arc.java
@@ -0,0 +1,11 @@
+package graphe;
+
+public class Arc {
+    private String dest;
+    private double cout;
+
+    public Arc(String dest, double cout) {
+        this.dest = dest;
+        this.cout = cout;
+    }
+}
