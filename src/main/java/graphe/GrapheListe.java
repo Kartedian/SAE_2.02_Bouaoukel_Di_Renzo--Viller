beb0d84d93f602a6bfdb96faa79842a889c267f2
Kartedian <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-05-22 13:11:05 +0200

    Add toString() methode

--- src/main/java/graphe/GrapheListe.java
+++ src/main/java/graphe/GrapheListe.java
@@ -6,19 +6,38 @@ public class GrapheListe {
     private ArrayList<String> noeuds;
     private ArrayList<Arcs> adjacence;
 
+    public GrapheListe(){
+        this.noeuds=new ArrayList<String>();
+        this.adjacence=new ArrayList<Arcs>();
+    }
+
     public int getIndice(String n){
-        if(noeuds.contains(n))
-            return noeuds.indexOf(n);
+        if(this.noeuds.contains(n))
+            return this.noeuds.indexOf(n);
         else
             return -1;
     }
 
-    public void ajouterArc(String depart, String destination, double cout){
-        if(destination == null && destination.isEmpty() || depart == null && depart.isEmpty() || cout<=0.0) {
+    public void ajouterArc(String depart, String destination, double cout) {
+        if (destination == null && destination.isEmpty() || depart == null && depart.isEmpty() || cout <= 0.0) {
             return;
         }
-        if(!this.noeuds.contains(depart))
+        if (!this.noeuds.contains(depart)) {
             this.noeuds.add(depart);
-        this.adjacence.get(getIndice(depart)).ajouterArc(new Arc(destination, cout));
+            this.adjacence.add(new Arcs());
+        }
+        this.adjacence.get(this.getIndice(depart)).ajouterArc(new Arc(destination, cout));
+    }
+
+    public String toString(){
+        String res="";
+        for(String noeud : this.noeuds){
+            res+=noeud+" -> ";
+            for(Arc arc : this.adjacence.get(getIndice(noeud)).getArcs()){
+                res+= arc.toString() + " ";
+            }
+            res+="\n";
+        }
+        return res;
     }
 }
