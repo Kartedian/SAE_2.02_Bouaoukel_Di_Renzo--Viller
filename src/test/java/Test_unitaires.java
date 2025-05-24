52ae761d3888a5382bcfc92fbec4c3f2c2a7e5d0
Bouaoukel Walid <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-05-24 16:09:45 +0200

    Test_Bellman ajoute

--- src/test/java/Test_unitaires.java
+++ src/test/java/Test_unitaires.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Test_unitaires {
 
     @Test
-    public void Test_Arc_1() {
+    public void Test_Arc() {
         Arc x = new Arc("A", 10);
 
         assertEquals("A(10.0)", x.toString());
@@ -19,7 +19,7 @@ public class Test_unitaires {
     }
 
     @Test
-    public void Test_Graphe_1() {
+    public void Test_Graphe() {
         GrapheListe x = new GrapheListe();
         x.ajouterArc("A", "B", 10);
         x.ajouterArc("B", "C", 10);
@@ -29,6 +29,38 @@ public class Test_unitaires {
         assertEquals(1, x.getIndice("B"));
         assertEquals(2, x.getIndice("C"));
         assertEquals(0, x.getIndice("A"));
+
+        assertEquals(1, x.suivants("A").size());
+        assertNull(x.suivants("Y"));
+
+        assertEquals(3, x.listeNoeuds().size());
     }
 
+    @Test
+    public void Test_Bellman() {
+        GrapheListe gra = new GrapheListe();
+
+        gra.ajouterArc("A", "B", 7);
+        gra.ajouterArc("A", "F", 10);
+        gra.ajouterArc("B", "C", 8);
+        gra.ajouterArc("B", "A", 2);
+        gra.ajouterArc("C", "D", 7);
+        gra.ajouterArc("C", "B", 4);
+        gra.ajouterArc("D", "E", 4);
+        gra.ajouterArc("D", "C", 2);
+        gra.ajouterArc("E", "F", 4);
+        gra.ajouterArc("E", "D", 2);
+        gra.ajouterArc("F", "A", 1);
+        gra.ajouterArc("F", "E", 2);
+
+
+
+        Valeurs res = Bellman.resoudre(gra, "A");
+        assertEquals(res.getValeur("A"), 0.0);
+        assertEquals(res.getValeur("B"), 7.0);  //A>B
+        assertEquals(res.getValeur("C"), 15.0); //A>B>C
+        assertEquals(res.getValeur("D"), 14.0); //A>F>E>D
+        assertEquals(res.getValeur("E"), 12.0); //A>F>E
+        assertEquals(res.getValeur("F"), 10.0); //A>F
+    }
 }
