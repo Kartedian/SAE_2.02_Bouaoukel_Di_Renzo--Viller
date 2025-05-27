d07aa8082777616429dcdf9b4206bac67ed9a3bb
Kartedian <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-05-27 13:01:36 +0200

    Correction Table graphe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,13 @@ import graphe.*;
 public class Main {
     public static void main(String[] args) {
         GrapheListe gra = new GrapheListe();
-        gra.ajouterArc("D", "B", 23);
-        gra.ajouterArc("B", "E", 12);
-        gra.ajouterArc("B", "C", 10);
-        gra.ajouterArc("C", "A", 19);
-        gra.ajouterArc("A", "B", 12);
-        gra.ajouterArc("E", "D", 43);
-        gra.ajouterArc("E", "D", 87);
+        gra.ajouterArc("A","B",12);
+        gra.ajouterArc("A","D",87);
+        gra.ajouterArc("B","E",11);
+        gra.ajouterArc("C","A",19);
+        gra.ajouterArc("D","B",23);
+        gra.ajouterArc("D","C",10);
+        gra.ajouterArc("E","D",43);
 
         System.out.println(gra.toString());
 
